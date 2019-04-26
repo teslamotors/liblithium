48aa34252aa7ac34999927fb1c666fbbc3a0919f
Chris Copeland <chris@chrisnc.net>
2019-04-26 04:06:42 -0700

    fix gimli test output

--- test.c
+++ test.c
@@ -9,18 +9,11 @@ int main()
 
   for (i = 0;i < 12;++i) x[i] = i * i * i + i * 0x9e3779b9;
 
-  for (i = 0;i < 12;++i) {
-    printf("%08x ",x[i]);
-    if (i % 4 == 3) printf("\n");
-  }
-  printf("----------------------\n");
-
   gimli(x);
 
   for (i = 0;i < 12;++i) {
     printf("%08x ",x[i]);
     if (i % 4 == 3) printf("\n");
   }
-  printf("----------------------\n");
   return 0;
 }
