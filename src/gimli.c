e3572ca665687dc6350a35c8cc240f153ed8e61d
Chris Copeland <chris@chrisnc.net>
2019-06-19 15:04:16 -0700

    reduce scope of gimli variables, add swap function

--- src/gimli.c
+++ src/gimli.c
@@ -9,24 +9,25 @@ static uint32_t rotate(uint32_t x, int n)
     return (x << n) | (x >> (32 - n));
 }
 
+static void swap(uint32_t *x, uint32_t *y)
+{
+    const uint32_t tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
 void gimli(uint32_t *state)
 {
-    unsigned int round;
-    unsigned int column;
-    uint32_t x;
-    uint32_t y;
-    uint32_t z;
-
-    for (round = 24; round > 0; --round)
+    for (int round = 24; round > 0; --round)
     {
-        for (column = 0; column < 4; ++column)
+        for (int column = 0; column < 4; ++column)
         {
-            x = rotate(state[column], 24);
-            y = rotate(state[4 + column], 9);
-            z = state[8 + column];
+            const uint32_t x = rotate(state[column], 24);
+            const uint32_t y = rotate(state[column + 4], 9);
+            const uint32_t z = state[column + 8];
 
-            state[8 + column] = x ^ (z << 1) ^ ((y & z) << 2);
-            state[4 + column] = y ^ x ^ ((x | z) << 1);
+            state[column + 8] = x ^ (z << 1) ^ ((y & z) << 2);
+            state[column + 4] = y ^ x ^ ((x | z) << 1);
             state[column] = z ^ y ^ ((x & y) << 3);
         }
 
@@ -34,24 +35,16 @@ void gimli(uint32_t *state)
         {
         case 0:
             // small swap: pattern s...s...s... etc.
-            x = state[0];
-            state[0] = state[1];
-            state[1] = x;
-            x = state[2];
-            state[2] = state[3];
-            state[3] = x;
+            swap(&state[0], &state[1]);
+            swap(&state[2], &state[3]);
             // add constant: pattern c...c...c... etc.
-            state[0] ^= (0x9e377900 | round);
+            state[0] ^= (UINT32_C(0x9e377900) | (uint32_t)round);
             break;
 
         case 2:
             // big swap: pattern ..S...S...S. etc.
-            x = state[0];
-            state[0] = state[2];
-            state[2] = x;
-            x = state[1];
-            state[1] = state[3];
-            state[3] = x;
+            swap(&state[0], &state[2]);
+            swap(&state[1], &state[3]);
             break;
         }
     }
