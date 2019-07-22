e139f2cb07b76fcb929e24e0ecc2e442af0f47e7
Chris Copeland <chris@chrisnc.net>
2019-07-22 15:07:51 -0700

    #elif -> #else

--- src/gimli_hash.c
+++ src/gimli_hash.c
@@ -6,7 +6,7 @@
 #if defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) &&             \
     (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__) && (CHAR_BIT == 8)
 #define LITH_LITTLE_ENDIAN 1
-#elif
+#else
 #define LITH_LITTLE_ENDIAN 0
 #endif
 
