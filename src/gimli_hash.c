336ee4cdedbd7d6ebc606067399772dcf09ebff5
Chris Copeland <chris@chrisnc.net>
2019-07-22 19:41:33 -0700

    optimize gimli_hash_update

--- src/gimli_hash.c
+++ src/gimli_hash.c
@@ -10,22 +10,34 @@
 #define LITH_LITTLE_ENDIAN 0
 #endif
 
+#if defined(__OPTIMIZE_SIZE__) || defined(_OPTIMIZE_FOR_SPACE)
+#define LITH_SMALL 1
+#else
+#define LITH_SMALL 0
+#endif
+
 unsigned char gimli_read8(const uint32_t *state, size_t i)
 {
-#if (LITH_LITTLE_ENDIAN)
-    return ((const unsigned char *)state)[i];
-#else
-    return (unsigned char)(state[i / 4] >> (8 * (i % 4))) & 0xFFU;
-#endif
+    if (LITH_LITTLE_ENDIAN)
+    {
+        return ((const unsigned char *)state)[i];
+    }
+    else
+    {
+        return (unsigned char)((state[i / 4] >> (8 * (i % 4))) & 0xFFU);
+    }
 }
 
 void gimli_xor8(uint32_t *state, size_t i, unsigned char x)
 {
-#if (LITH_LITTLE_ENDIAN)
-    ((unsigned char *)state)[i] ^= x;
-#else
-    state[i / 4] ^= (uint32_t)x << (8 * (i % 4));
-#endif
+    if (LITH_LITTLE_ENDIAN)
+    {
+        ((unsigned char *)state)[i] ^= x;
+    }
+    else
+    {
+        state[i / 4] ^= (uint32_t)x << (8 * (i % 4));
+    }
 }
 
 void gimli_hash_init(gimli_hash_state *g)
@@ -33,8 +45,7 @@ void gimli_hash_init(gimli_hash_state *g)
     memset(g, 0, sizeof(*g));
 }
 
-void gimli_hash_update(gimli_hash_state *g, const unsigned char *input,
-                       size_t len)
+static void update(gimli_hash_state *g, const unsigned char *input, size_t len)
 {
     size_t offset = g->offset;
     for (size_t i = 0; i < len; ++i)
@@ -48,6 +59,36 @@ void gimli_hash_update(gimli_hash_state *g, const unsigned char *input,
         }
     }
     g->offset = offset;
+}
+
+#define GIMLI_RATE_WORDS (GIMLI_RATE / 4)
+
+void gimli_hash_update(gimli_hash_state *g, const unsigned char *input,
+                       size_t len)
+{
+    if (LITH_LITTLE_ENDIAN && !LITH_SMALL)
+    {
+        const size_t first_block_len = (GIMLI_RATE - g->offset) % GIMLI_RATE;
+        if (len >= GIMLI_RATE + first_block_len)
+        {
+            update(g, input, first_block_len);
+            input += first_block_len;
+            len -= first_block_len;
+            do
+            {
+                uint32_t block[GIMLI_RATE_WORDS];
+                memcpy(block, input, GIMLI_RATE);
+                for (size_t i = 0; i < GIMLI_RATE_WORDS; ++i)
+                {
+                    g->state[i] ^= block[i];
+                }
+                gimli(g->state);
+                input += GIMLI_RATE;
+                len -= GIMLI_RATE;
+            } while (len >= GIMLI_RATE);
+        }
+    }
+    update(g, input, len);
 }
 
 void gimli_hash_final(gimli_hash_state *g, unsigned char *output, size_t len)
