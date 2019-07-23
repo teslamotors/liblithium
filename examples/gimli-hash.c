77282fb8e8b6b3a230fceca3cd1f51f0484d1dfc
Chris Copeland <chris@chrisnc.net>
2019-07-23 14:18:30 -0700

    change gimli-hash example to read from a file

--- examples/gimli-hash.c
+++ examples/gimli-hash.c
@@ -1,21 +1,42 @@
 #include <lithium/gimli_hash.h>
 
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        fprintf(stderr, "usage: %s <input-file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     unsigned char output[32];
 
     gimli_hash_state state;
     gimli_hash_init(&state);
 
+    int msgfd = open(argv[1], O_RDONLY);
+    if (msgfd < 0)
+    {
+        perror("open");
+        return EXIT_FAILURE;
+    }
+
     static unsigned char buf[4096];
     ssize_t nread;
 
-    while ((nread = read(STDIN_FILENO, buf, sizeof buf)) > 0)
+    while ((nread = read(msgfd, buf, sizeof buf)) > 0)
     {
         gimli_hash_update(&state, buf, (size_t)nread);
+    }
+
+    if (nread < 0)
+    {
+        perror("read");
+        return EXIT_FAILURE;
     }
 
     gimli_hash_final(&state, output, sizeof output);
