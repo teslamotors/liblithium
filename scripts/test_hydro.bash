9b1879d13d334a2cc20385eb6fc80a99df7c216f
Chris Copeland <chris@chrisnc.net>
2019-10-30 15:39:00 -0700

    create scripts directory and add test_hydro.bash to check compatibility with libhydrogen

---
+++ scripts/test_hydro.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xe
+
+scons --jobs $(nproc)
+
+
+testdir="dist/test/hydrotest"
+PATH="$(pwd)/dist/hydro:$PATH"
+
+mkdir -p "$testdir"
+pushd "$testdir"
+
+hydro-keygen testkey
+head -c 10000000 /dev/urandom > input.bin
+cat input.bin > /dev/null
+time hydro-sign testkey input.bin input.sig
+hydro-verify testkey.pub input.bin input.sig
+
+[ -d libhydrogen ] || git clone https://github.com/jedisct1/libhydrogen.git
+
+clang -O3 -flto -o libhydrogen-sign -Ilibhydrogen libhydrogen/hydrogen.c ../../../hydro/examples/hydro-sign.c
+clang -O3 -flto -o libhydrogen-verify -Ilibhydrogen libhydrogen/hydrogen.c ../../../hydro/examples/hydro-verify.c
+
+# check that libhydrogen can verify signatures from hydro
+./libhydrogen-verify testkey.pub input.bin input.sig
+
+# sign with libhydrogen and check that hydro can verify
+time ./libhydrogen-sign testkey input.bin input_libhydrogen.sig
+hydro-verify testkey.pub input.bin input_libhydrogen.sig
