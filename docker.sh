63256377eb04e962733a160aeef5b682e4356127
Chris Copeland <chris@chrisnc.net>
2021-08-23 15:15:45 -0700

    modify docker.sh to use a named container

--- docker.sh
+++ docker.sh
@@ -4,20 +4,23 @@ set -xe
 
 name="lithium"
 image="$name-builder"
+container="$name-container"
 
 docker build --tag "$image" .
 
-container=$(docker create --tty \
-    --volume "$(pwd):/src/$name" \
-    --cap-add SYS_PTRACE \
-    "$image" \
-    "bash" "-c" \
-    "git clone $name $name-docker && cd $name-docker && ./build.sh")
-
-if docker start --interactive "$container"; then
-    docker cp "$container:/src/$name-docker/dist" "dist"
+if docker run \
+  --name "$container" \
+  --tty \
+  --interactive \
+  --volume "$(pwd):/src/$name" \
+  --cap-add SYS_PTRACE \
+  "$image" \
+  "bash" "-c" \
+  "git clone $name $name-docker && cd $name-docker && ./build.sh"
+then
+  docker cp "$container:/src/$name-docker/dist" "dist"
 else
-    echo "Build failed, skipping copy out of docker container."
+  echo "Build failed, skipping copy out of docker container."
 fi
 
 docker rm "$container"
