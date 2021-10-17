ac58eef261e727af2792c3f54895272faa365f2e
Chris Copeland <chris@chrisnc.net>
2021-10-16 19:05:34 -0700

    use a variable message length in TrustInSoft entrypoints

--- .trustinsoft/sign.c
+++ .trustinsoft/sign.c
@@ -1,11 +1,14 @@
 #include <lithium/random.h>
 #include <lithium/sign.h>
 
+#include <tis_builtin.h>
+
 int main(void)
 {
     unsigned char public_key[LITH_SIGN_PUBLIC_KEY_LEN],
-        secret_key[LITH_SIGN_SECRET_KEY_LEN], sig[LITH_SIGN_LEN], msg[50];
+        secret_key[LITH_SIGN_SECRET_KEY_LEN], sig[LITH_SIGN_LEN], msg[1024];
     lith_sign_keygen(public_key, secret_key);
     lith_random_bytes(msg, sizeof msg);
-    lith_sign_create(sig, msg, sizeof msg, secret_key);
+    size_t msglen = tis_unsigned_long_interval(0, sizeof msg);
+    lith_sign_create(sig, msg, msglen, secret_key);
 }
--- .trustinsoft/verify.c
+++ .trustinsoft/verify.c
@@ -1,12 +1,15 @@
 #include <lithium/random.h>
 #include <lithium/sign.h>
 
+#include <tis_builtin.h>
+
 int main(void)
 {
     unsigned char public_key[LITH_SIGN_PUBLIC_KEY_LEN],
-        secret_key[LITH_SIGN_SECRET_KEY_LEN], sig[LITH_SIGN_LEN], msg[50];
+        secret_key[LITH_SIGN_SECRET_KEY_LEN], sig[LITH_SIGN_LEN], msg[1024];
+    size_t msglen = tis_unsigned_long_interval(0, sizeof msg);
     lith_sign_keygen(public_key, secret_key);
     lith_random_bytes(msg, sizeof msg);
     lith_random_bytes(sig, sizeof sig);
-    return !lith_sign_verify(sig, msg, sizeof msg, public_key);
+    return !lith_sign_verify(sig, msg, msglen, public_key);
 }
